995be6bf0911a2c28f6539fb4dfd6c0ab876931f
ADOLFO SAUL <you@example.com>
2018-03-22 11:26:27 -0600

    formulario

---
+++ saul/Form5.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace saul
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
+            System.Windows.Forms.Label nombreLabel;
+            System.Windows.Forms.Label claveLabel;
+            this.basesdatos1DataSet = new saul.basesdatos1DataSet();
+            this.algebraBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.algebraTableAdapter = new saul.basesdatos1DataSetTableAdapters.algebraTableAdapter();
+            this.tableAdapterManager = new saul.basesdatos1DataSetTableAdapters.TableAdapterManager();
+            this.algebraBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.algebraBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.claveTextBox = new System.Windows.Forms.TextBox();
+            nombreLabel = new System.Windows.Forms.Label();
+            claveLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.basesdatos1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.algebraBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.algebraBindingNavigator)).BeginInit();
+            this.algebraBindingNavigator.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // basesdatos1DataSet
+            // 
+            this.basesdatos1DataSet.DataSetName = "basesdatos1DataSet";
+            this.basesdatos1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // algebraBindingSource
+            // 
+            this.algebraBindingSource.DataMember = "algebra";
+            this.algebraBindingSource.DataSource = this.basesdatos1DataSet;
+            // 
+            // algebraTableAdapter
+            // 
+            this.algebraTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.algebraTableAdapter = this.algebraTableAdapter;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.calculoTableAdapter = null;
+            this.tableAdapterManager.fisicaTableAdapter = null;
+            this.tableAdapterManager.quimicaTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = saul.basesdatos1DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // algebraBindingNavigator
+            // 
+            this.algebraBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.algebraBindingNavigator.BindingSource = this.algebraBindingSource;
+            this.algebraBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.algebraBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.algebraBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.algebraBindingNavigatorSaveItem});
+            this.algebraBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.algebraBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.algebraBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.algebraBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.algebraBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.algebraBindingNavigator.Name = "algebraBindingNavigator";
+            this.algebraBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.algebraBindingNavigator.Size = new System.Drawing.Size(800, 25);
+            this.algebraBindingNavigator.TabIndex = 0;
+            this.algebraBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Mover primero";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Mover anterior";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Posición";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Posición actual";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(37, 15);
+            this.bindingNavigatorCountItem.Text = "de {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Número total de elementos";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Mover siguiente";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Mover último";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Agregar nuevo";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Eliminar";
+            // 
+            // algebraBindingNavigatorSaveItem
+            // 
+            this.algebraBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.algebraBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("algebraBindingNavigatorSaveItem.Image")));
+            this.algebraBindingNavigatorSaveItem.Name = "algebraBindingNavigatorSaveItem";
+            this.algebraBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.algebraBindingNavigatorSaveItem.Text = "Guardar datos";
+            this.algebraBindingNavigatorSaveItem.Click += new System.EventHandler(this.algebraBindingNavigatorSaveItem_Click);
+            // 
+            // nombreLabel
+            // 
+            nombreLabel.AutoSize = true;
+            nombreLabel.Location = new System.Drawing.Point(434, 106);
+            nombreLabel.Name = "nombreLabel";
+            nombreLabel.Size = new System.Drawing.Size(45, 13);
+            nombreLabel.TabIndex = 1;
+            nombreLabel.Text = "nombre:";
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.algebraBindingSource, "nombre", true));
+            this.nombreTextBox.Location = new System.Drawing.Point(485, 103);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nombreTextBox.TabIndex = 2;
+            // 
+            // claveLabel
+            // 
+            claveLabel.AutoSize = true;
+            claveLabel.Location = new System.Drawing.Point(434, 132);
+            claveLabel.Name = "claveLabel";
+            claveLabel.Size = new System.Drawing.Size(36, 13);
+            claveLabel.TabIndex = 3;
+            claveLabel.Text = "clave:";
+            // 
+            // claveTextBox
+            // 
+            this.claveTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.algebraBindingSource, "clave", true));
+            this.claveTextBox.Location = new System.Drawing.Point(485, 129);
+            this.claveTextBox.Name = "claveTextBox";
+            this.claveTextBox.Size = new System.Drawing.Size(100, 20);
+            this.claveTextBox.TabIndex = 4;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(nombreLabel);
+            this.Controls.Add(this.nombreTextBox);
+            this.Controls.Add(claveLabel);
+            this.Controls.Add(this.claveTextBox);
+            this.Controls.Add(this.algebraBindingNavigator);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.basesdatos1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.algebraBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.algebraBindingNavigator)).EndInit();
+            this.algebraBindingNavigator.ResumeLayout(false);
+            this.algebraBindingNavigator.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private basesdatos1DataSet basesdatos1DataSet;
+        private System.Windows.Forms.BindingSource algebraBindingSource;
+        private basesdatos1DataSetTableAdapters.algebraTableAdapter algebraTableAdapter;
+        private basesdatos1DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator algebraBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton algebraBindingNavigatorSaveItem;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.TextBox claveTextBox;
+    }
+}
---
+++ saul/Form5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saul
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void algebraBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.algebraBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.basesdatos1DataSet);
+
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'basesdatos1DataSet.algebra' Puede moverla o quitarla según sea necesario.
+            this.algebraTableAdapter.Fill(this.basesdatos1DataSet.algebra);
+
+        }
+    }
+}
